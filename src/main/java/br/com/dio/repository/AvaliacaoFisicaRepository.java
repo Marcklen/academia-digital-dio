fed28954c13b1b2a54ad7fee67463e0aa89e24aa
Marcklen <marcklen@icloud.com>
2022-07-25 14:36:38 -0300

    adicionando os repositorios e arquivos da infra

---
+++ src/main/java/br/com/dio/repository/AvaliacaoFisicaRepository.java
@@ -0,0 +1,11 @@
+package br.com.dio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.dio.entity.AvaliacaoFisica;
+
+@Repository
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long>{
+
+}
